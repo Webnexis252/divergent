8387ec43fa51ee6e23ed2387737ee964d5ad061e
opalvedansh <vedanshlovesmom88@gmail.com>
2026-06-03 12:59:01 +0530

    Clean up test scripts

--- test-db.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "./src/lib/prisma";
-async function main() {
-  console.log("Connecting...");
-  const user = await prisma.user.findFirst();
-  console.log("Success:", user?.id);
-}
-main().catch(console.error).finally(() => process.exit(0));
--- test-users.ts
+++
@@ -1,6 +0,0 @@
-import prisma from "./src/lib/prisma";
-async function main() {
-  const users = await prisma.user.findMany({ select: { id: true, name: true, role: true } });
-  console.log(users);
-}
-main().catch(console.error).finally(() => process.exit(0));
